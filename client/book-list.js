39e6f12ce2061ec80f849aab0ca3276b998cd568
tuguldur1219 <81338394+tuguldur1219@users.noreply.github.com>
2023-05-01 17:42:52 +0800

    Add files via upload

--- client/book-list.js
+++ client/book-list.js
@@ -1,73 +1,81 @@
-const setEditModal = (isbn) => {
-    const xhttp = new XMLHttpRequest();
+const { MongoClient } = require('mongodb');
 
-    xhttp.open("GET", `http://localhost:3000/book/${isbn}`, false);
-    xhttp.send();
+const url = 'mongodb://localhost:27017';
+const dbName = 'library';
 
-    const book = JSON.parse(xhttp.responseText);
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
-    const {
-        title, 
-        author, 
-        publisher, 
-        publish_date,
-        numOfPages
-    } = book;
+async function run() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
 
-    document.getElementById('isbn').value = isbn;
-    document.getElementById('title').value = title;
-    document.getElementById('author').value = author;
-    document.getElementById('publisher').value = publisher;
-    document.getElementById('publish_date').value = publish_date;
-    document.getElementById('numOfPages').value = numOfPages;
+    const db = client.db(dbName);
+    const books = db.collection('books');
 
-    // setting up the action url for the book
-    document.getElementById('editForm').action = `http://localhost:3000/book/${isbn}`;
-}
+    // Insert a new book
+    const result = await books.insertOne({
+      isbn: '1234567890',
+      title: 'The Great Gatsby',
+      author: 'F. Scott Fitzgerald',
+      publisher: 'Scribner',
+      publish_date: new Date(1925, 3, 10),
+      numOfPages: 218,
+    });
 
-const deleteBook = (isbn) => {
-    const xhttp = new XMLHttpRequest();
+    console.log(`Inserted ${result.insertedCount} document(s)`);
 
-    xhttp.open("DELETE", `http://localhost:3000/book/${isbn}`, false);
-    xhttp.send();
+    // Update a book
+    const filter = { isbn: '1234567890' };
+    const updateDoc = {
+      $set: {
+        author: 'Francis Scott Fitzgerald',
+        publish_date: new Date(1925, 4, 10),
+        numOfPages: 250,
+      },
+    };
+    const updateResult = await books.updateOne(filter, updateDoc);
 
-    location.reload();
-}
+    console.log(`Updated ${updateResult.modifiedCount} document(s)`);
 
-const loadBooks = () => {
-    const xhttp = new XMLHttpRequest();
+    // Delete a book
+    const deleteResult = await books.deleteOne({ isbn: '1234567890' });
+    console.log(`Deleted ${deleteResult.deletedCount} document(s)`);
 
-    xhttp.open("GET", "http://localhost:3000/book", false);
-    xhttp.send();
+    // Find all books and display them
+    const cursor = await books.find();
+    await cursor.forEach(book => {
+      const x = `
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${book.isbn}</h6>
 
-    const books = JSON.parse(xhttp.responseText);
+                    <div>Author: ${book.author}</div>
+                    <div>Publisher: ${book.publisher}</div>
+                    <div>Number Of Pages: ${book.numOfPages}</div>
 
-    for (let book of books) {
-        const x = `
-            <div class="col-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">${book.title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${book.isbn}</h6>
+                    <hr>
 
-                        <div>Author: ${book.author}</div>
-                        <div>Publisher: ${book.publisher}</div>
-                        <div>Number Of Pages: ${book.numOfPages}</div>
-
-                        <hr>
-
-                        <button type="button" class="btn btn-danger" onclick="deleteBook(${book.isbn})">Delete</button>
-                        <button types="button" class="btn btn-primary" data-toggle="modal" 
-                            data-target="#editBookModal" onClick="setEditModal(${book.isbn})">
-                            Edit
-                        </button>
-                    </div>
+                    <button type="button" class="btn btn-danger" onclick="deleteBook('${book.isbn}')">Delete</button>
+                    <button types="button" class="btn btn-primary" data-toggle="modal" 
+                        data-target="#editBookModal" onClick="setEditModal('${book.isbn}')">
+                        Edit
+                    </button>
                 </div>
             </div>
-        `
+        </div>
+      `;
 
-        document.getElementById('books').innerHTML = document.getElementById('books').innerHTML + x;
-    }
+      document.getElementById('books').innerHTML = document.getElementById('books').innerHTML + x;
+    });
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+    console.log('Disconnected from MongoDB');
+  }
 }
 
-loadBooks();
+run();
